63c0ac8a615c83a3dcfbe1f3e4b1fbe4ad7159d5
Nochang0 <hy040504@kakao.com>
2026-03-22 16:57:01 +0900

    2Grade Logic-Circuit update (2026-03-22 16:57:01)

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "커밋 메시지: " msg
+read -p "(논리회로) 커밋 메시지: " msg
 
 # 입력이 비었으면 기본 메시지 사용
 if [ -z "$msg" ]; then
-    msg="2Grade Logic-Circuit update"
+    msg="2Grade Logic-Circuit update ($(date '+%Y-%m-%d %H:%M:%S'))"
 fi
 
 git add .
